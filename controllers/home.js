da95af297dee968e1dd048bc66460a67ebe6d2b5
jcalandr <calandre.jordan@gmail.com>
2017-03-23 03:41:12 +0100

    maj metadata

--- controllers/home.js
+++ controllers/home.js
@@ -7,35 +7,33 @@ const mongoDAO = require('../dao/mongoDAO');
 const openDataDAO = require('../dao/openDataDAO');
 const url = require('../models/url');
 const tag = require('../models/tag');
-const metadata =require('../models/metadata.json');
+const metadata = require('../models/metadata.json');
 
 exports.index = function (req, res) {
-    res.render('home',{title:"Home"});
+    res.render('home', {title: "Home"});
 };
 
 exports.get_data = async(req, res) => {
     const request = req.query.url;
 
     const result = await openDataDAO.get(url[request]);
-    res.render('home',{title:"Home", items:result.opendata.answer.status});
+    res.render('home', {title: "Home", items: result.opendata.answer.status});
 };
 
 exports.get_id = async(req, res) => {
     const request = req.query.tag;
-    console.log(request);
-    let result=[];
 
-    for(let props in tag){
-        if((String(tag[props]).toLowerCase()).includes(request.toLowerCase())){
-            for (var i = 0; i < metadata.length; i++){
-                if (metadata[i].identifier == props){
-                    result.push(metadata[i])
-                }
-            }
+    let result = [];
+
+    for (let props in metadata) {
+        console.log(metadata[props].identifier);
+        if ((String(metadata[props].tags).toLowerCase()).includes(request.toLowerCase())) {
+            result.push(metadata[props])
         }
     }
     res.send(result)
-};
+}
+;
 
 
 exports.api_get_data = async(req, res) => {
@@ -48,13 +46,13 @@ exports.api_get_data = async(req, res) => {
 /**
  * POST /
  */
-exports.post_data = function(req,res){
+exports.post_data = function (req, res) {
 
     console.log("post_data");
-    var date=new Date();
+    var date = new Date();
     var item = {
         name: req.body.name,
-        date: date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear(),
+        date: date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear(),
         mail: req.body.mail,
         value: req.body.value
     };
@@ -68,26 +66,26 @@ exports.post_data = function(req,res){
 /**
  * UPDATE /
  */
-exports.update_data = function(req,res){
-    Commentaire.findOne({name: req.body.name}, function(err, doc) {
+exports.update_data = function (req, res) {
+    Commentaire.findOne({name: req.body.name}, function (err, doc) {
         if (err) {
             console.error('error, no entry found');
         }
         doc.content = req.body.content;
         doc.mail = req.body.mail;
-        doc.value =  req.body.value;
+        doc.value = req.body.value;
         doc.save();
     });
     res.redirect('/');
 };
 
-exports.delete_data = function(req,res){
+exports.delete_data = function (req, res) {
 
-    Commentaire.findOneAndRemove({mail: req.body.mail}, function(err, doc) {
+    Commentaire.findOneAndRemove({mail: req.body.mail}, function (err, doc) {
         if (err) throw err;
-        if(doc){
+        if (doc) {
             console.log('Doc found and removed');
-        }else{
+        } else {
             console.log('No user found');
         }
     });
